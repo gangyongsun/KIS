0316283fc0ab0206992befd1318209a2c3f07bf8
AlvinSun <gangyongsun@163.com>
2019-12-19 17:09:39 +0800

    添加新功能

--- src/main/java/cn/com/goldwind/kis/repository/NonSearchRepository.java
+++
@@ -1,8 +0,0 @@
-package cn.com.goldwind.kis.repository;
-
-import cn.com.goldwind.kis.entity.KeyInfoNonSearch;
-import tk.mybatis.mapper.common.Mapper;
-
-public interface NonSearchRepository extends Mapper<KeyInfoNonSearch>{
-
-}
